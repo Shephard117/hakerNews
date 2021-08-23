91a2c75650d2d1ba348de5f75c1af0d0307efae8
Dmitry <legovaz91@gmail.com>
2021-08-23 11:16:39 +0700

    Initial Commit

---
+++ Haker news/Haker_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haker_newsApp.swift
+//  Haker news
+//
+//  Created by Дмитрий Скоробогаты on 23.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Haker_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
